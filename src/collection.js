f4b18fce408cce6e2babeef66e13ef741ab447a4
Yannis Torres <yann.torres@gmail.com>
2016-09-15 00:19:47 +0200

    chore(doc): started to comment classes

--- src/collection.js
+++ src/collection.js
@@ -1,17 +1,33 @@
 import { resolver } from 'aurelia-dependency-injection';
 import { Config } from './config';
 
+/**
+ * Collection class. Resolver for services injection.
+ */
 @resolver()
 export class Collection {
-
+  /**
+   * Collection resolver constructor.
+   * @param  {String} key : should be the unique key that identify a service instance.
+   */
   constructor(key) {
     this._key = key;
   }
 
+  /**
+   * Accessor to the services instance through aurelia container.
+   * @param  {Container} container : the aurelia container.
+   * @return {Service} The service instance associated to the collection key, otherwise undefined.
+   */
   get(container) {
     return container.get(Config).getCollection(this._key);
   }
 
+  /**
+   * Static acces to a service instance, this should be used in injections as Collection.of('collection-key')
+   * @param  {String} key : the unique key of that identify a service.
+   * @return {Collection} A collection resolver.
+   */
   static of(key) {
     return new Collection(key);
   }
--- src/config.js
+++ src/config.js
@@ -8,11 +8,17 @@ function ObjectCreator(data) {
 }
 
 /**
- * Config class. Configures and stores collections services
+ * Config class. Configures and stores collections instances.
  */
 @inject(Aurelia, HttpClient)
 export class Config {
 
+  /**
+   * [constructor description]
+   * @param  {[type]} aurelia    [description]
+   * @param  {[type]} httpClient [description]
+   * @return {[type]}            [description]
+   */
   constructor(aurelia, httpClient) {
     this.httpClient = httpClient;
     this.aurelia = aurelia;
@@ -21,6 +27,15 @@ export class Config {
   collections = {};
   defaultCollection = null;
 
+  /**
+   * [registerCollection description]
+   * @param  {[type]} key               [description]
+   * @param  {[type]} defaultRoute      [description]
+   * @param  {[type]} collectionService [description]
+   * @param  {[type]} modelClass        [description]
+   * @param  {String} modelid           [description]
+   * @return {[type]}                   [description]
+   */
   registerCollection(key, defaultRoute, collectionService, modelClass = ObjectCreator, modelid = '_id') {
     this.collections[key] = collectionService;
     collectionService.configure(this.aurelia.container, this, key, defaultRoute, modelClass, modelid);
@@ -30,6 +45,11 @@ export class Config {
     return this;
   }
 
+  /**
+   * [getCollection description]
+   * @param  {[type]} key [description]
+   * @return {[type]}     [description]
+   */
   getCollection(key) {
     if (!key) {
       return this.defaultCollection || null;
@@ -38,10 +58,20 @@ export class Config {
     return this.collections[key] || null;
   }
 
+  /**
+   * [collectionExists description]
+   * @param  {[type]} key [description]
+   * @return {[type]}     [description]
+   */
   collectionExists(key) {
     return !!this.collections[key];
   }
 
+  /**
+   * [setDefaultCollection description]
+
+   * @param {[type]} key [description]
+   */
   setDefaultCollection(key) {
     this.defaultCollection = this.getCollection(key);
 
