1ee3427551e5ac8793e98da4e14697839253235e
chabou-san <chabou@live.com>
2017-02-19 18:47:54 +0100

    fix(test): Use of StageComponent to ensure aurelia’s framework is loaded as expected.

--- test/unit/use-collection.spec.js
+++ test/unit/use-collection.spec.js
@@ -1,14 +1,32 @@
+import { Container } from 'aurelia-dependency-injection';
+import { StageComponent } from 'aurelia-testing';
+import { bootstrap } from 'aurelia-bootstrapper';
+
 import { Config } from '../../src/config';
 import { Collection } from '../../src/collection';
 import { UseCollection } from '../../src/use-collection';
-import { Container } from 'aurelia-dependency-injection';
 import { InjectTest } from './resources/inject-test';
 
-let container = new Container();
-let config = container.get(Config);
-config.registerCollection('fake', 'http://jsonplaceholder.typicode.com');
-
 describe('UseCollection', () => {
+  let component;
+  let container;
+  let config;
+
+  beforeAll(done => {
+    component = StageComponent.withResources();
+    component.create(bootstrap)
+      .then(() => {
+        container = Container.instance;
+        config = container.get(Config);
+        config.registerCollection('fake', 'http://jsonplaceholder.typicode.com');
+        done();
+      });
+  });
+
+  afterAll(() => {
+    component.dispose();
+  });
+
   describe('static .of()', () => {
     it('Should return a new instance of self.', () => {
       let resolver = UseCollection.of('foo');
