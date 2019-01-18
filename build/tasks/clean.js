42d5cc936ceb515b28a121f544a610e9541ae8ba
Yannis Torres <yann.torres@gmail.com>
2019-01-18 01:26:52 +0100

    feat(all): removed v0.x configuration

--- build/tasks/clean.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp';
-import del from 'del';
-
-import paths from '../paths';
-
-const clean = () => del([paths.output]);
-export { clean };
-gulp.task('clean', clean);
--- test/unit/resources/inject-test.js
+++
@@ -1,9 +0,0 @@
-import { inject } from 'aurelia-dependency-injection';
-import { UseCollection } from '../../../src/use-collection';
-
-@inject(UseCollection.of('fake'))
-export class InjectTest {
-  constructor(myCollection) {
-    this.myCollection = myCollection;
-  }
-}
